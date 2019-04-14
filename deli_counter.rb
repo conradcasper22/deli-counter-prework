24dbed556be994446995a1c2451b52ec7d1c8e6b
Conrad Casper <conradcasper@icloud.com>
2019-04-14 16:56:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,12 @@ require 'pry'
 
 def line(katz_deli)
   other_deli = ["The line is currently: "]
-  binding.pry
+  
   if katz_deli == []
    puts "The line is currently empty."
   else
     katz_deli.each_with_index {|person, index| other_deli.push("#{index}. #{person} ")}
   end
 other_deli.join()
+binding.pry
 end
