7ad10705676847931f3146f3619ede2a6a1fa599
Conrad Casper <conradcasper@icloud.com>
2019-04-14 15:46:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,6 @@
 def line(katz_deli)
   if katz_deli == []
     "The line is currently empty."
+  else
+    katz_deli.each_with_index {|person, index| "The line is currently: #{index + 1}. #{person}"}
 end
